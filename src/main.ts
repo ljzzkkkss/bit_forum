c118c13b6b4e08bb7aa96cc3060547efa164621e
liujun <793958447@qq.com>
2017-03-13 12:33:03 +0800

    干他娘的bootstrap-loader

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
-import './third-part/bootstrap.min.css'
+import './third-part/bootstrap.min.css';
 if (process.env.ENV === 'production') {
   enableProdMode();
 }
