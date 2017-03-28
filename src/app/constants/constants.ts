316982131c6263a7f2ee505bf8957d5a1e86cb8d
Ciki_Flame <18694942447@163.com>
2017-03-29 00:21:00 +0800

    干他娘的响应式owl-carousel

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,3 +1,3 @@
 export class Constants{
-  static url : string = process.env.ENV === 'production' ? 'http://180.76.167.170:8088' : 'http://localhost:3000';
+  static url : string = process.env.ENV !== 'production' ? 'http://180.76.167.170:8088' : 'http://localhost:3000';
 }
