d8ec8d0772ae205825d1b6f340fd3422a1156cac
Ciki_Flame <18694942447@163.com>
2017-07-12 16:01:16 +0800

    bug

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -7,17 +7,18 @@ import {Router} from "@angular/router";
 
 import '../../../third-part/owlcarousel/owl.carousel.min.js';
 import {HttpService} from "../../service/http.service";
-import {Constants} from "../../constants/constants";
-import {CookieUtil} from "../../util/cookie.util";
 import '../../../third-part/jquery/jquery.countTo.js';
+import '../../../third-part/jquery/wow.min.js';
 
 declare var $ : any;
+declare var WOW : any;
 
 @Component({
     selector: 'homepage',
     templateUrl: 'homepage.component.html',
     styleUrls: [
         'homepage.component.css',
+        '../../../third-part/css/animate.css',
         '../../../third-part/owlcarousel/assets/owl.carousel.min.css',
         '../../../third-part/owlcarousel/assets/owl.theme.default.min.css'
     ]
@@ -42,6 +43,12 @@ export class HomepageComponent implements  OnInit{
                 $("#scrollUp").fadeOut(200);
             }
         });
+
+        var wow = new WOW({
+            animateClass: 'animated',
+            offset: 120
+        });
+        wow.init();
 
     }
 
